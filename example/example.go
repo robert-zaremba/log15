ecb6dea503150fa0d7bd2f1936d1b57911b2e593
Robert Zaremba <robert.zaremba@scale-it.pl>
2015-11-12 12:40:40 +0100

    Added example file

---
+++ example/example.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"gopkg.in/inconshreveable/log15.v2"
+)
+
+func main() {
+	log15.Debug("page accessed", "path", "/a/bbb/c")
+	log15.Info("page accessed", "path", "/a/bbb/c")
+	log15.Warn("page accessed", "path", "/a/bbb/c")
+	log15.Error("page accessed", "path", "/a/bbb/c")
+	log15.Crit("page accessed", "path", "/a/bbb/c")
+}
